3f07a25a4d9bc0afb2c3425a570caab88577e7b6
David Bohan <madvlad00@googlemail.com>
2018-09-09 22:35:12 -0400

    Initial commit

---
+++ TimerFrame/Assets/Scripts/Mocks/MockCoreManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Mocks
+{
+    static class MockCoreManager
+    {
+        public static bool WonGame = false;
+        public static float CurrentTimeScale = 1f;
+    }
+}
