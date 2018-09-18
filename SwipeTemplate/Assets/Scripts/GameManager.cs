fbdcaa254982dde935980f2885076e9ba03301ac
David Bohan <madvlad00@googlemail.com>
2018-09-17 23:23:21 -0400

    Add swipe template

---
+++ SwipeTemplate/Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.Mocks;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    public bool HasWon = false;
+
+    private void Start()
+    {
+        Time.timeScale = MockCoreManager.CurrentTimeScale;
+    }
+
+    void EndGame()
+    {
+        if (HasWon)
+        {
+            MockCoreManager.WonGame = true;
+        }
+    }
+}
---
+++ SwipeTemplate/Assets/Scripts/Mocks/MockCoreManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Mocks
+{
+    static class MockCoreManager
+    {
+        public static bool WonGame = false;
+        public static float CurrentTimeScale = 1f;
+		public static int difficulty = 0;
+    }
+}
